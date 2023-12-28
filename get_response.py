de4ac23bdfd9bf0d12c0a7693431737bbb7a595c
hanwool95 <hanwool95@snu.ac.kr>
2023-12-28 17:50:09 +0900

    FEATURE: 여러 파일을 작업할 수 있도록 반영

--- get_response.py
+++ get_response.py
@@ -1,10 +1,49 @@
+import csv
 import requests
 
 import config
 
-resp = requests.get(
-    'https://openapi.vito.ai/v1/transcribe/'+ config.TEST_TRANSCRIBE_ID,
-    headers={'Authorization': 'bearer '+config.SECRET_KEY},
-)
-resp.raise_for_status()
-print(resp.json())
+def save_result(csv_name, updated_data):
+    # 결과를 새로운 CSV 파일로 저장
+    with open(csv_name, 'w', newline='', encoding='utf-8') as file:
+        writer = csv.writer(file)
+        for row in updated_data:
+            writer.writerow(row)
+
+def update_csv_with_transcriptions(input_csv, output_csv, secret_key):
+    # CSV 파일 읽기
+    with open(input_csv, 'r') as file:
+        reader = csv.reader(file)
+        transcribe_ids = [row[0] for row in reader]
+
+    # 결과를 저장할 리스트 초기화
+    updated_data = []
+
+    count = 0
+    try:
+        # 각 transcribe ID에 대해 API 호출 및 결과 저장
+        for transcribe_id in transcribe_ids:
+            print(count)
+
+            response = requests.get(
+                f'https://openapi.vito.ai/v1/transcribe/{transcribe_id}',
+                headers={'Authorization': f'Bearer {secret_key}'}
+            )
+            response.raise_for_status()
+            result = response.json()
+
+            # 각 utterance에 대한 정보 저장
+            for utterance in result['results']['utterances']:
+                updated_data.append([result['id'], utterance['spk'], utterance['msg']])
+            count += 1
+    except:
+        save_result(output_csv, updated_data)
+
+    # 결과를 새로운 CSV 파일로 저장
+    save_result(output_csv, updated_data)
+
+
+# 이 함수를 사용하여 'output.csv' 파일을 읽고 각 ID에 대한 응답을 받아 'updated_output.csv'에 저장
+# config.SECRET_KEY를 여기에 직접 입력하거나 다른 방식으로 제공해야 합니다.
+update_csv_with_transcriptions('output.csv', 'updated_output.csv', config.SECRET_KEY)
+
--- stt_return_zero.py
+++ stt_return_zero.py
@@ -1,13 +1,57 @@
+import os
+import csv
 import json
 import requests
 import config
 
-api_config = {'use_diarization': True, 'domain': 'CALL', 'diarization': {'spk_count': 2}}
-resp = requests.post(
-    'https://openapi.vito.ai/v1/transcribe',
-    headers={'Authorization': 'bearer '+ config.SECRET_KEY},
-    data={'config': json.dumps(api_config)},
-    files={'file': open('voice_files/bosalpi2_202309151809586_050803510027_01051992168_01063571191.mp3', 'rb')}
-)
-resp.raise_for_status()
-print(resp.json())
+
+# 폴더 내의 모든 파일을 찾는 함수
+def find_files(directory):
+    return [os.path.join(directory, file) for file in os.listdir(directory) if file.endswith('.mp3')]
+
+
+# API 호출 및 ID 추출 함수
+def call_api(file_path, api_config):
+    with open(file_path, 'rb') as file:
+        resp = requests.post(
+            'https://openapi.vito.ai/v1/transcribe',
+            headers={'Authorization': 'bearer ' + config.SECRET_KEY},
+            data={'config': json.dumps(api_config)},
+            files={'file': file}
+        )
+        resp.raise_for_status()
+        return resp.json().get('id')
+
+
+# 메인 프로세스
+def process_files(directory):
+    api_config = {'use_diarization': True, 'domain': 'CALL', 'diarization': {'spk_count': 2}}
+    file_paths = find_files(directory)
+    ids = []
+
+    count = 0
+    try:
+        for file_path in file_paths:
+            id = call_api(file_path, api_config)
+            if id:
+                ids.append(id)
+            count += 1
+            print(count)
+            print(id)
+    except:
+        print(ids)
+        # CSV 파일로 저장
+        with open('output.csv', 'w', newline='') as csvfile:
+            csvwriter = csv.writer(csvfile)
+            for id in ids:
+                csvwriter.writerow([id])
+
+    # CSV 파일로 저장
+    with open('output.csv', 'w', newline='') as csvfile:
+        csvwriter = csv.writer(csvfile)
+        for id in ids:
+            csvwriter.writerow([id])
+
+
+# 'voice_files' 폴더의 파일들을 처리
+process_files('voice_files')
